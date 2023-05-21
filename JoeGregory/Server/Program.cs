62442a060789aa22d9f1fd5c51d04f6e9a5176fa
joe-gregory <josephedward2nd@gmail.com>
2023-05-20 17:33:09 -0700

    Commented out connections to db

--- JoeGregory/Server/Program.cs
+++ JoeGregory/Server/Program.cs
@@ -16,10 +16,10 @@ namespace JoeGregory
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlServer(connectionString));
+            //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             //builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
               //  .AddEntityFrameworkStores<ApplicationDbContext>();
